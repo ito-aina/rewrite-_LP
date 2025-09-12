3e5d8028e8007e1f2a55e624d79e9831e137ee3b
ito-aina <ithamamatsu.ito.aina@gmail.com>
2025-09-12 14:46:21 +0900

    全体ページ・ヘッダー動き完成、スライドショー未完成

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ faqItems.forEach(item => {
     item.classList.toggle('active');
   });
 });
+
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  if (window.scrollY > 100) {
+    header.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+  } else {
+    header.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+  }
+});
+
